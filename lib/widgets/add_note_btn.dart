e40ee2181795553e1e77abca9c83e7c3301b1663
ejim favour <favourejim56@gmail.com>
2024-11-01 08:39:08 +0100

    created the add note button

---
+++ lib/widgets/add_note_btn.dart
@@ -0,0 +1,38 @@
+import 'package:feather_icons/feather_icons.dart';
+import 'package:flutter/material.dart';
+
+class AddNoteBtn extends StatelessWidget {
+  const AddNoteBtn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 30,
+      right: 0,
+      child: InkWell(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(100),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.25),
+                spreadRadius: 5,
+                blurRadius: 10,
+                offset: const Offset(5, 5),
+              ),
+            ],
+          ),
+          child: const Icon(
+            FeatherIcons.plus,
+            color: Colors.white,
+            size: 50,
+          ),
+        ),
+      ),
+    );
+  }
+}
