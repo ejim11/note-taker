9200c6760fa608a28989479076e354e72ff7c79f
ejim favour <favourejim56@gmail.com>
2024-11-01 07:54:07 +0100

    added the notes array

---
+++ lib/data/notes.dart
@@ -0,0 +1,52 @@
+import '../models/note.dart';
+
+List<Note> notes = [
+  Note(
+    title: 'To-do list',
+    note:
+        '1. Reply to emails, Prepare presentation slides for the marketing meeting, Conduct research on competitor products Schedule and plan',
+    category: 'To-do list',
+    createdAt: DateTime(2024, 11, 1),
+    updatedAt: DateTime(2024, 11, 1),
+  ),
+  Note(
+    title: 'Home work',
+    note:
+        'Read chapters 5 for Psychology class.2. Write a 2-page reflection on the assigned literature eading for English class.',
+    category: 'Important',
+    createdAt: DateTime(2024, 11, 2),
+    updatedAt: DateTime(2024, 11, 3),
+  ),
+  Note(
+    title: 'In the late evening',
+    note:
+        "I retreated to my dorm room, finding solace in the quiet moments. Sometimes, I'd call home to catch up with my family, reassuring them that I'm doing well and",
+    category: 'dairy',
+    createdAt: DateTime(2024, 11, 3),
+    updatedAt: DateTime(2024, 11, 3),
+  ),
+  Note(
+    title: 'Afternoon classes',
+    note:
+        "classes were equally demanding, but I felt more at ease, thanks to the familiar faces I've come to know throughout my time here.",
+    category: 'Lecture Notes',
+    createdAt: DateTime(2024, 11, 4),
+    updatedAt: DateTime(2024, 11, 4),
+  ),
+  Note(
+    title: 'Campus tour',
+    note:
+        "I joined the sea of students bustling around, all with different destinations in mind. The campus was alive with energy, as everyone seemed to be on a ...",
+    category: 'To-do list',
+    createdAt: DateTime(2024, 11, 5),
+    updatedAt: DateTime(2024, 11, 6),
+  ),
+  Note(
+    title: "Food rollercoaster",
+    note:
+        "Food Rollercoater', note: 'juggling between college life's demands and the desire to savor every moment of this exhilarating journ woke up to the bl sound of my alarm",
+    category: 'dairy',
+    createdAt: DateTime(2024, 11, 6),
+    updatedAt: DateTime(2024, 11, 6),
+  )
+];
