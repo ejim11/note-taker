420c54b6f7bbe01cdb05e3fb1fe3fc633a69598d
ejim favour <favourejim56@gmail.com>
2024-11-03 12:45:02 +0100

    added id to the note model

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,17 +1,21 @@
-class Note {
-  Note({
-    required this.title,
-    required this.note,
-    this.category,
-    required this.createdAt,
-    required this.updatedAt,
-    this.isTagged = false,
-  });
+import "package:uuid/uuid.dart";
 
+const uuid = Uuid();
+
+class Note {
+  Note(
+      {required this.title,
+      required this.note,
+      this.category,
+      required this.createdAt,
+      required this.updatedAt,
+      String? id})
+      : id = id ?? uuid.v4();
+
+  final String id;
   final String title;
   final String note;
   final String? category;
   final DateTime createdAt;
   final DateTime updatedAt;
-  final bool isTagged;
 }
