4ca46e75067b47f387489da00a80a970c4a80454
ejim favour <favourejim56@gmail.com>
2024-11-04 23:45:48 +0100

    added the month and year to the date model

--- lib/models/date.dart
+++ lib/models/date.dart
@@ -1,10 +1,16 @@
 class Date {
-  Date({required this.day, required this.date});
+  Date(
+      {required this.day,
+      required this.date,
+      required this.month,
+      required this.year});
 
   final days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
   final int day;
   final int date;
+  final int month;
+  final int year;
 
   String getDay() {
     return days[day - 1];
